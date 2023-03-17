270121d51d9ce2a02da2a62d14e5d9cd5662f301
OhJongMin <jmoh7480@gmail.com>
2023-03-17 15:36:02 +0900

    File Input/Output

--- week1/Image-processing/File_InputOutput.c
+++ week1/Image-processing/File_InputOutput.c
@@ -3,6 +3,18 @@
 #include <stdlib.h>
 
 int main() {
+	FILE* fp1 = fopen("input.txt", "rb");
+	FILE* fp2 = fopen("output.txt", "wb");
+
+	unsigned char* ch = (unsigned char*)malloc(sizeof(unsigned char));
+
+	for (int i = 0; i < 12; i++) {
+		fread(ch, sizeof(unsigned char), 1, fp1);
+		fwrite(ch, sizeof(unsigned char), 1, fp2);
+	}
+
+	free(ch);
+	fclose(fp1); fclose(fp2);
 
 	return 0;
 }
